3e6fa11ea6dcc1dab77429083bad26fa7d565ae5
bqdillo2 <bqdillo2@gmail.com>
2026-07-09 16:11:21 -0500

    "Reto 1 – Mi primera presentación en GitHub"

---
+++ presentacion.py
@@ -0,0 +1,10 @@
+nombre = "Ewald"
+grado = "11°"
+colegio = "Francisco José de Caldas"
+lenguaje = "Python 🐍"
+print("=" * 45)
+print(f" Hola GitHub! Soy {nombre}")
+print(f" Grado: {grado} | Institución: {colegio}")
+print(f" Aprendiendo: {lenguaje}")
+print(f" ¡Este es mi primer commit! ")
+print("=" * 45)
